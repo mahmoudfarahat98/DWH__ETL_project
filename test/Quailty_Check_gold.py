61de3be19111642e25b680220a9cb7b65f5687eb
Mahmoud Farahat <155580382+mahmoudfarahat98@users.noreply.github.com>
2025-10-06 02:48:02 +0300

    Update Quailty_Check_gold.py

--- test/Quailty_Check_gold.py
+++ test/Quailty_Check_gold.py
@@ -17,130 +17,84 @@ Usage Notes:
 ===============================================================================
 """
 
-#----------------------------- #### Quality check  of silver crm cust info ------------------------------#
+#----------------------------- #### Quality check  of Customer_Dim ------------------------------#
 """
                         1.    Quality Check 
 """
 ####--------------------------------------------------------------##
 
-silver_dict = {}
-silver_dict["silver.crm_Cust_info"] = crm_Cust_info
+# Check if we have duplicates in customer info [ Expected No result]
 
-# # Check the duplicate [Expected: No result ]
-silver_dict["silver.crm_Cust_info"].groupBy("cst_id")\
-.agg(count("*").alias("cnt")).filter(col("cnt") > 1).show()       #  No result
+customer_dim.groupBy("customer_number").agg(count("*").alias("c"))\
+    .filter(col("c")>1).show()
 
-# to detect column errors [Expected: No result ]
-silver_dict["silver.crm_Cust_info"].filter(col("cst_firstname") != trim(col("cst_firstname"))).show() # >>> No result
-silver_dict["silver.crm_Cust_info"].filter(col("cst_lastname") != trim(col("cst_lastname"))).show() # >>> No result
+# Check is_current  [ Expected No result]
 
-# detect convert low_cardinality [Expected: No Cardinality column ]
-silver_dict["silver.crm_Cust_info"].select("cst_gndr").distinct().show()
-silver_dict["silver.crm_Cust_info"].select("cst_marital_status").distinct().show()
+customer_dim.filter((col("end_date").isNotNull())\
+                    & (col("is_current")== 0)).show()
+                    
 
+customer_dim.filter((col("end_date").isNull())\
+                    & (col("is_current")== 0)).show()
 
-#----------------------------- #### Quality check  of silver crm prd info ------------------------------#
+# Check Gender  [ Expected Male - Female - n/a]
+
+customer_dim.select("Gender").distinct().show()
+
+# Check Data_Types
+customer_dim.printSchema()
+
+# Check SK  [ Expected 18484, equal count rows]
+
+print(customer_dim.count())                                     #18484
+customer_dim.agg(max("cust_Sk")).show()                         #18484
+
+#----------------------------- #### Quality check  of Product_Dim ------------------------------#
 """
                       2.      Quality Check 
 """
 ####--------------------------------------------------------------##
 
- # Assign the table into silver dict
-silver_dict["silver.crm_prd_info"] = crm_prd_info
-print(silver_dict.keys())
+# Check Data_Types
+customer_dim.printSchema()
 
-# check duplicate in primary key [Expected No Result]
-silver_dict["silver.crm_prd_info"].groupBy("prd_id")\
-.agg(count("*").alias("cnt")).filter(col("cnt") > 1).show() # >>> No result 
+# Show the product with null category [Expected 7 rows Null in 4 column]
+product_dim.filter(col("Category").isNull()).show()
 
-# Check for unwanted spaces [Expected No Result]
-silver_dict["silver.crm_prd_info"].filter(col("prd_nm") != trim(col("prd_nm"))).show() # >>> No result 
+# Show the Category with null produced [Expected 1 row Null in 6]           # if use Full_Join
+product_dim.filter(col("prd_id").isNull()).show()
 
-# Check for Nulls or Negative Numbers [Expected No Result]
-silver_dict["silver.crm_prd_info"].filter((col("prd_cost") < 0) | (col("prd_cost").isNull())).show()
+# Check is_current column [Expected: No result ]
+product_dim.filter((col("start_dt").isNotNull())& (col("end_dt").isNotNull()))\
+    .filter(col("Is_Current")==1).show()          # No result
 
-# Check for Data Standardization $ Consistency [Expected No low-cardinality character]
-silver_dict["silver_crm_prd_info"].select("prd_line").distinct().show()
-
-# Check Invalid * Date * Order [expected No Result - No End < Start Date]
-silver_dict["silver.crm_prd_info"].select("prd_start_dt","prd_end_dt").filter((col("prd_start_dt") > col("prd_end_dt")) \
-                                                                              & col("prd_end_dt").isNotNull()).show()
-# overview
-silver_dict["silver.crm_prd_info"].show()
-
-#----------------------------- #### Quality check  of silver crm sales details ------------------------------#
+#----------------------------- #### Quality check  of Fact_sales ------------------------------#
 """
                      3.       Quality Check 
 """
 ####--------------------------------------------------------------##
-# check  Null, negative, or zero  [Expected No Result]
-crm_sales_details.filter((col("sls_order_dt") > col("sls_ship_dt"))\
-                                    | (col("sls_ship_dt")> col("sls_due_dt"))).show()
 
-# check  Null, negative, or zero  [Expected No Result]
-crm_sales_details.select("*").filter((col("sls_sales") <= 0) | (col("sls_sales").isNull())).show() 
+# 1- Fact Surggote Key [Expected the same number]
+fact_sales.agg(max("Sales_Sk")).show()           # 60398                
+print(fact_sales.count())                        # 60398
 
-# check Null, negative, or zero  [Expected No Result]
-crm_sales_details.select("*").filter((col("sls_price") <= 0) | (col("sls_price").isNull())).show() 
+# 2- Dimensions SK [Expected customer with many orders] ex: Customer_sk =  26 
+fact_sales.groupBy("Customer_FK").agg(count("*").alias("c")).filter(col("c")>1).limit(1).show()
 
-# check the error in calculation  [Expected No Result]
-crm_sales_details.select("*").filter((col("sls_price") * col("sls_quantity")) != col("sls_sales")).show() 
+# 3- Customer Dimensions SK = 26 [Expected 6 rows]
+fact_sales.alias("f").join(customer_dim, fact_sales["Customer_FK"]== customer_dim["cust_Sk"],"inner")\
+.filter(col("cust_Sk")== 26).select("f.*").show()
 
-crm_sales_details.printSchema()
+# 4- Test the Customer sales [Expected 6577]
+fact_sales.alias("f").join(customer_dim, fact_sales["Customer_FK"]== customer_dim["cust_Sk"],"inner")\
+.filter(col("cust_Sk")== 26).agg(sum("Total_sales")).show()          # 6577
 
-                    # H.Assign the table into the silver dict
+# 5- Product Dimensions with high purchase [Expected Product_fk = 3, 4244 ]
+fact_sales.alias("f").join(product_dim, fact_sales["Product_FK"]== product_dim["Product_SK"],"inner")\
+.groupby("f.Product_FK").agg(count("Total_sales").alias("Hights_product")).orderBy(col("Hights_product").asc()).show()
 
-silver_dict["silver.crm_sales_details"]= crm_sales_details 
-print(silver_dict.keys())
+# 6- Tracking the CUSTOMER with no order [Expected No Result]
+fact_sales.alias("f").join(customer_dim.alias("c"), fact_sales["Customer_FK"]== customer_dim["cust_Sk"],  "left")\
+    .join(product_dim.alias("p"), fact_sales["Product_FK"]== product_dim["Product_SK"], "left")\
+    .filter((col("c.cust_Sk").isNull())|(col("p.Product_SK").isNull())).show()
 
-#-------------------------- #### Quality check  of silver Erp cust az12 ----------------------------#
-"""
-                   4.         Quality Check 
-"""
-####--------------------------------------------------------------##
-
-# check the all ID is in erp_cust is correct with crm_cust [Expected No Result]
-erp_cust_az12.join(crm_cust, crm_cust["cst_key"]== erp_cust_az12["CID"], "left_anti").show() # no result [done] 
-
-# check the bdate is logic [!> current date] [Expected no ]
-erp_cust_az12.select("*").filter((col("BDATE") > current_date())).show()  # No result
-
-
-# check Gender is not null, trimmed, high-cardinality  [Expected Male, Female, N/a]
-
-erp_cust_az12.select("GEN").distinct().show()  
-
-                       # H.Assign the table into the silver dict
-
-silver_dict["silver.erp_cust_az12"]= erp_cust_az12 
-print(silver_dict.keys())
-
-#-------------------------- #### Quality check  of bronze erp loc a101"] ----------------------------#
-"""
-                 5.           Quality Check 
-"""
-####--------------------------------------------------------------##
-# Quality Check of silver erp _ loc
-
-# check the all ID is in erp_loc is correct with crm_cust [expected No Result]
-erp_loc_a101.join(crm_cust_2, erp_loc_a101["CID"]== crm_cust_2["cst_key"],"left_anti").show()
-
-
-
-# check Gender is not null, trimed , high-cardinality  [expected No Null or duplicate in meaning]
-
-erp_loc_a101.select("CNTRY").distinct().show()  
-
-                       # H.Assign the table into silver dict
-
-silver_dict["silver.erp_loc_a101"]= erp_loc_a101
-print(silver_dict.keys())
-
-#-------------------------- #### Quality check  of bronze erp loc a101"] ----------------------------#
-"""
-                 6.           Quality Check 
-"""
-####--------------------------------------------------------------##
-                            # Assign the table into silver dict
-silver_dict["silver.erp_px_cat_g1v2"] = erp_px_cat_g1v2
-print(silver_dict.keys())
